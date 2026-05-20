68a76b235f0643be8364b4dfd1385e4005ff19fc
TomekoYT <tomekoyt@interia.pl>
2026-05-20 11:38:58 +0200

    add bloom replacements

--- src/main/java/tomeko/healthvignette/utils/Constants.java
+++ src/main/java/tomeko/healthvignette/utils/Constants.java
@@ -1,6 +1,6 @@
 package tomeko.healthvignette.utils;
 
 public class Constants {
-    public static final String MOD_NAME = "Health Vignette";
-    public static final String MOD_ID = "healthvignette";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final String MOD_ID = "@MOD_ID@";
 }
